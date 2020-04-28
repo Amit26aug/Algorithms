aabf8affcf525bba7a825dbe283ca1c8f0141914
Amit Sharma <36073441+Amit26aug@users.noreply.github.com>
2020-04-28 17:50:07 +0530

    Update QuickUnionCode.java

--- Course/UnionFind/QuickUnionCode.java
+++ Course/UnionFind/QuickUnionCode.java
@@ -32,34 +32,40 @@ class QuickFind {
 class QuickFindCode {
 	public static void main (String[] args) throws java.lang.Exception {
 	    
+	    long startTime = System.currentTimeMillis();
+	    
 	    Scanner sc = new Scanner(System.in);
 	    int n = Integer.parseInt(sc.nextLine());
 	    
 	    QuickFind qu = new QuickFind(n);
 	    
 	    while(sc.hasNext()) {
-		    // input is of form op obj1 obj2
-		    // op can be 0 for union or 1 for find
-		    // for op 0, perform union of ob1 and ob2
-		    // for op 1, perform find of ob1 and ob2
-
-		    String[] input = sc.nextLine().trim().split(" ");
-		    int op = Integer.parseInt(input[0]);
-		    int obj1 = Integer.parseInt(input[1]);
-		    int obj2 = Integer.parseInt(input[2]);
-
-		    if(op == 0) {
-				qu.union(obj1, obj2);
-				System.out.println("Union: " + obj1 + " and " + obj2);
-		    }
-		    else if(op == 1) {
-				boolean connected = qu.find(obj1, obj2);
-				System.out.println("Connected: " + obj1 + " and " + obj2 + "-> " + connected);
-		    } 
-		    else {
-				throw new IllegalArgumentException();
-		    }
+            // input is of form op obj1 obj2
+            // op can be 0 for union or 1 for find
+            // for op 0, perform union of ob1 and ob2
+            // for op 1, perform find of ob1 and ob2
+            
+            String[] input = sc.nextLine().trim().split(" ");
+            int op = Integer.parseInt(input[0]);
+            int obj1 = Integer.parseInt(input[1]);
+            int obj2 = Integer.parseInt(input[2]);
+            
+            if(op == 0) {
+                qu.union(obj1, obj2);
+                System.out.println("Union: " + obj1 + " and " + obj2);
+            }
+            else if(op == 1) {
+                boolean connected = qu.find(obj1, obj2);
+                System.out.println("Connected: " + obj1 + " and " + obj2 + "-> " + connected);
+            } 
+            else {
+                throw new IllegalArgumentException();
+            }
 	    }
+	    
+	    long endTime = System.currentTimeMillis();
+	    
+	    System.out.println("Time: " + (endTime - startTime) + "ms");
 		
 	}
 }
